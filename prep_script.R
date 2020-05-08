7cd96ef00fb5c5b2e3e2820ff1fd650e17c7069e
Nav <naveed.ishaque@hotmail.co.uk>
2020-05-08 16:02:04 +0200

    add a CRAN mirror to avoid CLI error

--- prep_script.R
+++ prep_script.R
@@ -4,7 +4,7 @@
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg))
-    install.packages(new.pkg, dependencies = TRUE)
+    install.packages(new.pkg, dependencies = TRUE, repos='http://cran.us.r-project.org')
 }
 
 ipak_bc <- function(pkg){
