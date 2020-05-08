2b7dfe8e706a23ad01e83a8d88be8ae7f0e5ca85
Ahmed Sadik <ahmed.asadik@gmail.com>
2020-05-08 17:25:55 +0200

    Workflow sh file

---
+++ Workflow_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Run the AHR signature scripts first
+cd AHR_signature_scripts
+./IDO1_TDO2_associate_w_AHR_signature_activation.R
+./WGCNA_modules_Associating_w_AHR_activation_AAMs.R
+cd..
+
+## Run the IL4I1_scripts
+cd IL4I1_scripts
+./IL4I1_activates_AHR.R
+./IL4I1_and_AHR_activity_in_metastatic_and_primary_melanoma.R
+./IL4I1_immune_associations.R
+./IL4I1_in_TCGA_and_GTEX_tissues.R
+./IL4I1_survival_outcome_TCGA.R
+./Riaz_etal_ICB_dataset.R
+./Weisser_GSE58211_relapsed_CLL_dataset.R
+cd..
